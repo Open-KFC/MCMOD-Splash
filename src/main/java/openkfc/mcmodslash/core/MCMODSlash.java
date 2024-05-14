202680d452b32439cca303777d71deafec5aff4d
Open-KFC <1811593346@qq.com>
2024-05-14 23:02:32 +0800

    commit1

--- src/main/java/openkfc/mcmodslash/core/MCMODSlash.java
+++ src/main/java/openkfc/mcmodslash/core/MCMODSlash.java
@@ -3,16 +3,16 @@ package openkfc.mcmodslash.core;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod(
-        modid = MCMODSplash.MODID,
-        name = MCMODSplash.NAME,
-        version = MCMODSplash.VERSION,
+        modid = MCMODSlash.MODID,
+        name = MCMODSlash.NAME,
+        version = MCMODSlash.VERSION,
         clientSideOnly = true
 )
-public class MCMODSplash {
+public class MCMODSlash {
     public static final String MODID = "mcmodslash";
     public static final String NAME = "MCMOD-Slash";
     public static final String VERSION = "0.1.0";
     @Mod.Instance
-    public MCMODSplash instance;
+    public MCMODSlash instance;
 
 }
